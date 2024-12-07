6ccb0f7312ebc626a39eaf8032d64055737fcd90
NandlalShah955 <shahnandlal49@gmail.com>
2024-12-07 14:19:44 -0500

    achange

--- config/connectdb.js
+++ config/connectdb.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
+// Load environment variables from .env file
+dotenv.config();
 const connectDB = async (DATABASE_URL) => {
   try {
+    const DATABASE_URL = process.env.DATABASE_URL;
     const DB_OPTIONS = {
       dbName: "Pepper_cloud",
     };
